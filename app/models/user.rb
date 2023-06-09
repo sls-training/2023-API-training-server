8b0416a943abb5b53cd171162e93b35427824d13
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-06-09 16:15:38 +0900

    Remove redundant validations for the password attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   before_save { email.downcase! }
 
-  has_secure_password
-
-  # nil値は has_secure_password() と validate_presence_of() の両方で検知され、同じ内容のエラーが同時に複数発生する。
-  # これを防ぐために、オプションに allow_nil : true を指定して validate_presence_of() によるnil値の検出を回避する。
-  validates :password, presence: true, length: { minimum: 16, maximum: 128 }, allow_nil: true
+  has_secure_password validations: false
+  validates :password, presence: true, length: { minimum: 16, maximum: 128 }
 end
