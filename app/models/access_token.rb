b2c943303ab4021a1c6dc14ce65848303330b73d
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-06-15 13:16:34 +0900

    Remove an unnecessary validation

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -8,7 +8,7 @@ class AccessToken < ApplicationRecord
   #
   # 上記では、トークンの文字列長は1以上であればよいことになっているが、短すぎると総当たりで突破されないとも
   # 限らないので 保険として、 `=` によるパディング以外の部分に関して、ある程度の長さを要求する。
-  validates :token, presence: true, format: { with: %r{\A[\w\-.~+/]{16,}=*\Z} }, uniqueness: true
+  validates :token, format: { with: %r{\A[\w\-.~+/]{16,}=*\Z} }, uniqueness: true
   validates :scope, format: { with: /\A(READ|WRITE)( (READ|WRITE)*)\Z/ }
   validates :expires_in, numericality: { greater_than_or_equal_to: 0 }
 
