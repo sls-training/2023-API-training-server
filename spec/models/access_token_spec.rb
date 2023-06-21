50bc5bbc8d6ab3922ca79285ebf550277ac9f005
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-06-21 17:13:51 +0900

    Change some subjects to target method calls

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -24,19 +24,21 @@ RSpec.describe AccessToken, type: :model do
 
   describe '#valid?' do
     describe 'token' do
-      subject { described_class.new token:, user: }
+      subject { access_token.valid? }
+
+      let(:access_token) { described_class.new token:, user: }
 
       context 'Bearerトークンとして無効な文字列の場合' do
         let(:token) { "#{'a' * 15}@" }
 
-        it { is_expected.to be_invalid }
+        it { is_expected.to be_falsey }
       end
 
       context 'Bearerトークンとして有効な文字列の場合' do
         # パディングを除いてトークン文字列として有効な文字を全て含んだ文字列
         let(:token) { "#{(('a'..'z').to_a + ('A'..'Z').to_a + (1..9).to_a).join}-._~+/" }
 
-        it { is_expected.to be_valid }
+        it { is_expected.to be_truthy }
       end
 
       context '重複しているとき' do
@@ -46,125 +48,135 @@ RSpec.describe AccessToken, type: :model do
           FactoryBot.create :access_token, token:
         end
 
-        it { is_expected.to be_invalid }
+        it { is_expected.to be_falsey }
       end
     end
 
     describe 'scope' do
-      subject { described_class.new scope:, user: }
+      subject { access_token.valid? }
+
+      let(:access_token) { described_class.new scope:, user: }
 
       context 'READのとき' do
         let(:scope) { 'READ' }
 
-        it { is_expected.to be_valid }
+        it { is_expected.to be_truthy }
       end
 
       context 'WRITEのとき' do
         let(:scope) { 'WRITE' }
 
-        it { is_expected.to be_valid }
+        it { is_expected.to be_truthy }
       end
 
       context '正当な値がスペース区切りで連結しているとき' do
         let(:scope) { 'READ WRITE' }
 
-        it { is_expected.to be_valid }
+        it { is_expected.to be_truthy }
       end
 
       context '同じ正当な値が複数含まれているとき' do
         let(:scope) { 'READ READ' }
 
-        it { is_expected.to be_valid }
+        it { is_expected.to be_truthy }
       end
 
       context '適当な値のとき' do
         let(:scope) { Faker::Lorem.characters }
 
-        it { is_expected.to be_invalid }
+        it { is_expected.to be_falsey }
       end
 
       context '一部に正規のスコープの値を含んでいるとき' do
         let(:scope) { 'REWRITE' }
 
-        it { is_expected.to be_invalid }
+        it { is_expected.to be_falsey }
       end
 
       context 'スペース区切りになっていないとき' do
         let(:scope) { 'READ READWRITE' }
 
-        it { is_expected.to be_invalid }
+        it { is_expected.to be_falsey }
       end
     end
 
     describe 'expires_in' do
-      subject { described_class.new expires_in:, user: }
+      subject { access_token.valid? }
+
+      let(:access_token) { described_class.new expires_in:, user: }
 
       context '0のとき' do
         let(:expires_in) { 0 }
 
-        it { is_expected.to be_valid }
+        it { is_expected.to be_truthy }
       end
 
       context '-1のとき' do
         let(:expires_in) { -1 }
 
-        it { is_expected.to be_invalid }
+        it { is_expected.to be_falsey }
       end
     end
   end
 
   describe '#expired?' do
-    subject { described_class.new user: }
+    subject { access_token.expired? }
+
+    let(:access_token) { described_class.new user: }
 
     context '失効日時が未来のとき' do
       before do
-        subject.save!
+        access_token.save!
       end
 
-      it { is_expected.not_to be_expired }
+      it { is_expected.to be_falsey }
     end
 
     context '失効日時が過去のとき' do
       before do
         travel_to Date.current.yesterday do
-          subject.save!
+          access_token.save!
         end
       end
 
-      it { is_expected.to be_expired }
+      it { is_expected.to be_truthy }
     end
 
     context '永続化されていないとき' do
-      it { expect { subject.expired? }.to raise_error RuntimeError }
+      it { expect { subject }.to raise_error RuntimeError }
     end
   end
 
   describe '#revoked?' do
-    subject { described_class.new revoked_at:, user: }
+    subject { access_token.revoked? }
+
+    let(:access_token) { described_class.new revoked_at:, user: }
 
     context '無効化されているとき' do
       let(:revoked_at) { Time.current }
 
-      it { is_expected.to be_revoked }
+      it { is_expected.to be_truthy }
     end
 
     context '無効化されていないとき' do
       let(:revoked_at) { nil }
 
-      it { is_expected.not_to be_revoked }
+      it { is_expected.to be_falsey }
     end
   end
 
   describe '#revoke' do
-    subject { described_class.new user: }
+    subject { access_token.revoke }
+
+    let(:access_token) { described_class.new user: }
 
     context '無効化されているとき' do
       before do
-        subject.revoke
+        access_token.revoke
       end
 
       it 'revoked_atを更新しない' do
-        expect { subject.revoke }.not_to change(subject, :revoked_at)
+        expect { subject }.not_to change(access_token, :revoked_at)
       end
     end
 
@@ -174,7 +186,7 @@ RSpec.describe AccessToken, type: :model do
       end
 
       it 'revoked_atに無効化日時を設定する' do
-        expect { subject.revoke }.to change(subject, :revoked_at).from(nil).to(Time.current)
+        expect { subject }.to change(access_token, :revoked_at).from(nil).to(Time.current)
       end
     end
   end
