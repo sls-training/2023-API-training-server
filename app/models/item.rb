2e270c40b9e274140f4aa1adfe4463741decd313
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-06-28 10:28:25 +0900

    Switch to the lambda style to fallback the filename
    In this case, it's the most important to explain what the assign value
    has a mean in this application. So we only need to extract the value as
    a method. And the method name such as #set_xxx is a bad name for not
    setter methods.

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,11 @@ class Item < ApplicationRecord
   validates :description, length: { maximum: 1024 }
   validates :file, attached: true, size: { less_than_or_equal_to: 1.megabytes }
 
-  # TODO: コールバック以外に適切な方法があるかもしれない。
-  before_validation :set_fallback_filename, if: -> { name.blank? }
+  before_validation -> { self.name = original_filename }, if: -> { name.blank? }
 
   private
 
-  def set_fallback_filename
-    self.name = file&.filename
+  def original_filename
+    file&.filename
   end
 end
