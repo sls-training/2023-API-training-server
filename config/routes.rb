de411ddee89e30bff5d5022a7d14f2a21e484d93
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-06-29 17:08:52 +0900

    Add the action to revoke access tokens

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'user', to: 'users#create'
   post 'signin', to: 'access_tokens#create'
+  resource :access_tokens, as: :signout, only: %i[destroy]
 end
--- spec/requests/access_tokens_spec.rb
+++ spec/requests/access_tokens_spec.rb
@@ -107,4 +107,40 @@ RSpec.describe 'AccessTokens', type: :request do
       it { expect(subject.parsed_body).to include 'error' => 'invalid_scope' }
     end
   end
+
+  describe 'DELETE /signout' do
+    let(:user) { FactoryBot.create :user }
+    let(:access_token) { user.access_tokens.create! }
+
+    context '正当なパラメータを渡したとき' do
+      subject do
+        delete signout_path, params: { token: access_token.token }
+        response
+      end
+
+      it { is_expected.to have_http_status :success }
+
+      it '渡されたトークンに対応するアクセストークンが失効される' do
+        expect { subject }.to change { access_token.reload.revoked? }.from(false).to true
+      end
+    end
+
+    context '不正なパラメータを渡したとき' do
+      subject do
+        delete signout_path, params: { token: Faker::String.random }
+        response
+      end
+
+      it { is_expected.to have_http_status :bad_request }
+      it { is_expected.to have_attributes media_type: 'application/problem+json' }
+
+      it '発生したエラーに関する情報をProblem Details形式でボディにJSONとして含む' do
+        expect(subject.parsed_body).to include(
+          'status' => 400,
+          'title'  => 'Bad Request',
+          'error'  => 'invalid_request'
+        )
+      end
+    end
+  end
 end
