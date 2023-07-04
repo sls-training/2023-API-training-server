52f9da8c89094b00b406fc69e4039fccfdfed0ab
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-04 10:25:20 +0900

    Remove a shared_examples
    The shared_examples has some parameters and it's too complicated to
    share test examples and hard to maintenance it.

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -56,8 +56,105 @@ RSpec.describe 'Items', type: :request do
       end
     end
 
-    it_behaves_like 'OAuth 2.0のResource Owner Password Credentials Grantを利用したBearer Tokenによる認可を行う' do
-      let(:request_path) { files_path }
+    context '認証に必要なパラメータが欠落しているとき' do
+      subject do
+        post files_path
+        response
+      end
+
+      it { is_expected.to have_http_status :bad_request }
+      it { is_expected.to have_attributes media_type: 'application/problem+json' }
+
+      it '発生したエラーに関する情報がProblem Detailsに対応したJSON形式でボディに格納される' do
+        expect(subject.parsed_body).to include(
+          'status' => 400,
+          'title'  => 'Bad Request',
+          'error'  => 'invalid_request'
+        )
+      end
+    end
+
+    context 'アクセストークンが存在しないとき' do
+      subject do
+        post files_path, params: {}, headers: { Authorization: "Bearer #{Faker::Alphanumeric.alphanumeric}" }
+        response
+      end
+
+      it { is_expected.to have_http_status :unauthorized }
+      it { is_expected.to have_attributes media_type: 'application/problem+json' }
+
+      it '発生したエラーに関する情報がProblem Detailsに対応したJSON形式でボディに格納される' do
+        expect(subject.parsed_body).to include(
+          'status' => 401,
+          'title'  => 'Unauthorized',
+          'error'  => 'invalid_token'
+        )
+      end
+    end
+
+    context 'アクセストークンが無効化されているとき' do
+      subject do
+        post files_path, params: {}, headers: { Authorization: "Bearer #{access_token.token}" }
+        response
+      end
+
+      before do
+        access_token.revoke
+      end
+
+      it { is_expected.to have_http_status :unauthorized }
+      it { is_expected.to have_attributes media_type: 'application/problem+json' }
+
+      it '発生したエラーに関する情報がProblem Detailsに対応したJSON形式でボディに格納される' do
+        expect(subject.parsed_body).to include(
+          'status' => 401,
+          'title'  => 'Unauthorized',
+          'error'  => 'invalid_token'
+        )
+      end
+    end
+
+    context 'アクセストークンが失効しているとき' do
+      subject do
+        post files_path, params: {}, headers: { Authorization: "Bearer #{access_token.token}" }
+        response
+      end
+
+      before do
+        travel_to access_token.created_at + access_token.expires_in + 1
+      end
+
+      it { is_expected.to have_http_status :unauthorized }
+      it { is_expected.to have_attributes media_type: 'application/problem+json' }
+
+      it '発生したエラーに関する情報がProblem Detailsに対応したJSON形式でボディに格納される' do
+        expect(subject.parsed_body).to include(
+          'status' => 401,
+          'title'  => 'Unauthorized',
+          'error'  => 'invalid_token'
+        )
+      end
+    end
+
+    context 'アクセストークンの権限が不足しているとき' do
+      subject do
+        post files_path, params: {}, headers: { Authorization: "Bearer #{access_token.token}" }
+        response
+      end
+
+      let(:access_token) { user.access_tokens.create! scope: 'READ' }
+
+      it { is_expected.to have_http_status :forbidden }
+      it { is_expected.to have_attributes media_type: 'application/problem+json' }
+
+      it '発生したエラーに関する情報がProblem Detailsに対応したJSON形式でボディに格納される' do
+        expect(subject.parsed_body).to include(
+          'status' => 403,
+          'title'  => 'Forbidden',
+          'error'  => 'insufficient_scope',
+          'scope'  => 'WRITE'
+        )
+      end
     end
   end
 end
