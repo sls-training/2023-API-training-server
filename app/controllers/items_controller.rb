9b29f0b26e59d37648c8dc6a9de41cdfc40b8129
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-05 13:10:40 +0900

    Add the file index action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,16 @@
 class ItemsController < ApplicationController
   include Authorization
 
+  def index
+    files = current_user.files
+              .joins(:file_blob)
+              .order(index_orderby_param => index_order_param)
+              .page(index_page_param)
+              .per(index_per_param)
+
+    render json: ItemResource.new(files).serialize(root_key: :files)
+  end
+
   def create
     file = current_user.files.build file_params
 
@@ -17,5 +27,28 @@ class ItemsController < ApplicationController
 
   def file_params
     params.permit :name, :description, :file
+  end
+
+  def index_page_param
+    params[:page]
+  end
+
+  def index_per_param
+    params[:per]
+  end
+
+  def index_orderby_param
+    valid_orderbys = %i[id name description size created_at updated_at]
+    orderby = params[:orderby]&.to_sym
+
+    if valid_orderbys.any? orderby
+      orderby == :size ? 'active_storage_blobs.byte_size' : orderby
+    else
+      :created_at
+    end
+  end
+
+  def index_order_param
+    params.fetch(:order, 'asc') == 'desc' ? :desc : :asc
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :items, as: :files, only: %i[create]
+  resources :items, as: :files, only: %i[index create]
   resource :user, only: %i[create]
   resource :access_token, as: :signin, only: %i[create]
   resource :access_token_revocations, as: :signout, only: %i[create]
